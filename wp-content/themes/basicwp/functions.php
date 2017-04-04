32cf079b573b96f3a97da6541f71d0f7f6741646
jagas <mindaugas@weissmancooper.com>
2017-04-05 01:05:12 +0300

    columns in guests

--- wp-content/themes/basicwp/functions.php
+++ wp-content/themes/basicwp/functions.php
@@ -17,3 +17,23 @@ function myFunction()
 
 add_action('wp_ajax_myFunction', 'myFunction');
 add_action('wp_ajax_nopriv_myFunction', 'myFunction');
+
+add_filter('manage_edit-guest_columns', 'my_columns');
+function my_columns($columns) {
+    $columns['will_come'] = 'Will come';
+    return $columns;
+}
+
+add_action('manage_posts_custom_column',  'my_show_columns');
+function my_show_columns($name) {
+    global $post;
+    switch ($name) {
+        case 'will_come':
+            if(get_post_meta($post->ID, 'will_come', true)) {
+                $views = "Yes";
+            } else {
+                $views = "No";
+            }
+            echo $views;
+    }
+}
