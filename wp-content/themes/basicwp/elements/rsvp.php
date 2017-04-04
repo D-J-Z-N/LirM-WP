f62fb1649c5f896516438aac6755cb643bf4b9e9
jagas <mindaugas@weissmancooper.com>
2017-04-04 22:45:29 +0300

    refactored code, homepage, rsvp

---
+++ wp-content/themes/basicwp/elements/rsvp.php
@@ -0,0 +1,11 @@
+<section class="bottom pure-g" id="participate">
+    <div class="participation-form pure-u-1">
+        <label for="text"><p>Ar dalyvausite?</p></label>
+        <div class="input">
+            <input type="text" name="fname" placeholder="Mano vardas, pavardė" class="names">
+            <input type="submit" name"yes-submit" value="Taip" class="submit-button">
+            <input type="submit" name"no-submit" value="Ne" class="submit-button">
+        </div>
+        <a class="up-home" href="#top"></a>
+    </div>
+</section>
---
+++ wp-content/themes/basicwp/header.php
@@ -0,0 +1,18 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LirM</title>
+    <meta name="description" content="Vestes">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/base-min.css">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-min.css">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Wire+One&amp;subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>css/custom.css" type="text/css" media="all">
+
+</head>
